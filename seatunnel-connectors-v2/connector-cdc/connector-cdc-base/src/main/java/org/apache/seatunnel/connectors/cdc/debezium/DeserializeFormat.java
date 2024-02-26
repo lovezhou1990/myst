be06a4d92e3560e8310be9099c8229cedd62ed0b
lovezhou1990 <271218983@qq.com>
2024-02-26 09:46:27 +0800

    添加change_log 变更日志；

--- seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/debezium/DeserializeFormat.java
+++ seatunnel-connectors-v2/connector-cdc/connector-cdc-base/src/main/java/org/apache/seatunnel/connectors/cdc/debezium/DeserializeFormat.java
@@ -22,7 +22,7 @@ import org.apache.seatunnel.format.compatible.debezium.json.CompatibleDebeziumJs
 public enum DeserializeFormat {
     DEFAULT("default"),
     COMPATIBLE_DEBEZIUM_JSON(CompatibleDebeziumJsonDeserializationSchema.IDENTIFIER),
-    CHANGE_LOG("change_log");
+    CHANGE_LOG("changelog");
 
     private String name;
 
