9d6e28d15e83ec4c1aa9f2e80febe7a46283deae
271218983@qq.com <271218983@qq.com>
2024-08-25 21:45:53 +0800

    转换插件

---
+++ seatunnel-examples/seatunnel-engine-examples/src/main/java/org/apache/seatunnel/example/engine/BIZ_037_FSZLXK_ZY.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.seatunnel.example.engine;
+
+import org.apache.seatunnel.core.starter.SeaTunnel;
+import org.apache.seatunnel.core.starter.enums.MasterType;
+import org.apache.seatunnel.core.starter.exception.CommandException;
+import org.apache.seatunnel.core.starter.seatunnel.args.ClientCommandArgs;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class BIZ_037_FSZLXK_ZY {
+
+    public static void main(String[] args)
+            throws FileNotFoundException, URISyntaxException, CommandException {
+        String configurePath = args.length > 0 ? args[0] : "/examples/BIZ_037_FSZLXK_ZY.conf";
+        String configFile = getTestConfigFile(configurePath);
+        ClientCommandArgs clientCommandArgs = new ClientCommandArgs();
+        clientCommandArgs.setConfigFile(configFile);
+        clientCommandArgs.setCheckConfig(false);
+//        clientCommandArgs.setJobName(Paths.get(configFile).getFileName().toString());
+        // Change Execution Mode to CLUSTER to use client mode, before do this, you should start
+        // SeaTunnelEngineServerExample
+        clientCommandArgs.setAsync(true);
+        clientCommandArgs.setMasterType(MasterType.CLUSTER);
+        clientCommandArgs.setAsync(true);
+        SeaTunnel.run(clientCommandArgs.buildCommand());
+    }
+
+    public static String getTestConfigFile(String configFile)
+            throws FileNotFoundException, URISyntaxException {
+        URL resource = BIZ_037_FSZLXK_ZY.class.getResource(configFile);
+        if (resource == null) {
+            throw new FileNotFoundException("Can't find config file: " + configFile);
+        }
+        return Paths.get(resource.toURI()).toString();
+    }
+}
